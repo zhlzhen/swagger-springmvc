743e8243f1fbff88e127c8d1965e98041409b32b
Adrian Kelly <adrianbkelly@gmail.com>
2014-12-10 00:04:16 +1100

    [#51]6Tests parameter type serialization

--- swagger-models/src/test/groovy/com/wordnik/swagger/model/ParameterSpec.groovy
+++ swagger-models/src/test/groovy/com/wordnik/swagger/model/ParameterSpec.groovy
@@ -1,13 +1,32 @@
 package com.wordnik.swagger.model
 
+import spock.lang.Unroll
+
 class ParameterSpec extends InternalJsonSerializationSpec {
 
-  def "should serialize"() {
-
-    Parameter parameter = new Parameter("aname", "adesc", "defaultVal", false, false, "Date", null, "path", null)
+  @Unroll
+  def "should serialize dataType[#dataType]"() {
+    Parameter parameter = new Parameter("aname", "adesc", "defaultVal", false, false, dataType, null, "path", null)
     when:
       def json = writeAndParse(parameter)
     then:
-      json
+      assertion.call(json)
+
+    where:
+      dataType     | assertion
+      "int"        | { it.type == 'integer' && it.format == 'int32' }
+      "long"       | { it.type == 'integer' && it.format == 'int64' }
+      "float"      | { it.type == 'integer' && it.format == 'int64' }
+      "double"     | { it.type == 'number' && it.format == 'double' }
+      "Date"       | { it.type == 'string' && it.format == 'date-time' }
+      "DateTime"   | { it.type == 'string' && it.format == 'date-time' }
+      "string"     | { it.type == 'string' && !it.format }
+      "BigDecimal" | { it.type == 'number' && !it.format }
+      "BigInteger" | { it.type == 'number' && !it.format }
+      "boolean"    | { it.type == 'boolean' && !it.format }
+      "byte"       | { it.type == 'string' && it.format == 'byte' }
+      "UUID"       | { it.type == 'string' && it.format == 'uuid' }
+      "date"       | { it.type == 'string' && it.format == 'date' }
+      "date-time"  | { it.type == 'string' && it.format == 'date-time' }
   }
 }
